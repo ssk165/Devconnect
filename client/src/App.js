f4ee6fbf5aa9c07b9e43fb6ed6d2151828391a9a
Shubham Singh Kalyanwat <shubham16598@gmail.com>
2020-02-29 19:03:05 +0530

    register form data

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ import "./App.css";
 
 const App = () => (
   <Router>
-    <Fragment className="App">
+    <Fragment>
       <Navbar />
       <Route exact path="/" component={Landing} />
       <section className="container">
